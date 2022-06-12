02445bf69a9e76c9f31e227468aad7b79e74e038
Darkali-23 <syed0786ibu@gmail.com>
2022-06-12 13:28:23 +0530

    passport

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var session = require("express-session");
 var fileStore = require("session-file-store")(session);
 var logger = require("morgan");
 const mongoose = require("mongoose");
-
+var passport = require("passport");
+var authenticate = require("./authenticate");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
@@ -40,23 +41,17 @@ app.use(
     store: new fileStore(),
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 function auth(req, res, next) {
-  console.log(req.session);
-
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error("You are not authenticated!");
     err.status = 403;
     return next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 app.use(auth);
