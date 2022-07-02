cea4157c8ede7b445960e61e9d40e41e141dc545
Darkali-23 <syed0786ibu@gmail.com>
2022-07-02 21:19:18 +0530

    facebook oauth

--- authenticate.js
+++ authenticate.js
@@ -4,8 +4,10 @@ var User = require("./models/user");
 var JwtStrategy = require("passport-jwt").Strategy;
 var ExtractJwt = require("passport-jwt").ExtractJwt;
 var jwt = require("jsonwebtoken");
+var faceBookTokenStrategy = require("passport-facebook-token");
 
 var config = require("./config");
+const { use } = require("./app");
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -53,3 +55,34 @@ exports.verifyAdmin = (req, res, next) => {
     )
     .catch((err) => next(err));
 };
+
+exports.faceBookPassport = passport.use(
+  new faceBookTokenStrategy(
+    {
+      clientID: config.facebook.clientId,
+      clientSecret: config.facebook.clientSecret,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (!err && user !== null) {
+          return done(null, user);
+        } else {
+          user = new User({ username: profile.displayName });
+          user.facebookId = profile.id;
+          user.firstname = profile.name.givenName;
+          user.lastname = profile.name.familyName;
+          user.save((err, user) => {
+            if (err) {
+              return done(err, false);
+            } else {
+              return done(null, user);
+            }
+          });
+        }
+      });
+    }
+  )
+);
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "badhusha-2312",
   mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "530643285474908",
+    clientSecret: "79cc4950951e1e33f298649cfde48f3f",
+  },
 };
